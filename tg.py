b0efb1d7615d73c02d04cd926629275092e4bf0b
盐粒 Yanli <mail@yanli.one>
2023-10-29 14:19:20 +0800

    feat: support cookie pool

--- tg.py
+++ tg.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import telebot
+from typing import List
 from BingImageCreator import ImageGen
 from threading import Thread
 from telebot.types import InputMediaPhoto
@@ -9,10 +10,10 @@ from telebot.types import InputMediaPhoto
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("tg_token", help="tg token")
-    parser.add_argument("bing_cookie", help="bing cookie")
+    parser.add_argument("bing_cookie", help="bing cookie", nargs="+")
     options = parser.parse_args()
     bot = telebot.TeleBot(options.tg_token)
-    bing_cookie = options.bing_cookie
+    bing_cookie: List[str] = options.bing_cookie
     if not os.path.exists("tg_images"):
         os.mkdir("tg_images")
 
@@ -26,31 +27,46 @@ if __name__ == "__main__":
         prefix = next((w for w in start_words if message.text.startswith(w)), None)
         if not prefix:
             return
+        # Remove the first word if it is a @username, because it is part of the command if the bot is in a group chat.
+        s: str = message.text[len(prefix) :].strip()
+        if s.startswith("@"):
+            s = " ".join(s.split(" ")[1:])
+        # Prepare the local folder
         print(message.from_user.id)
         path = os.path.join("tg_images", str(message.from_user.id))
         if not os.path.exists(path):
             os.mkdir(path)
-        s: str = message.text[len(prefix) :].strip()
-        # Remove the first word if it is a @username, because it is part of the command if the bot is in a group chat.
-        if s.startswith("@"):
-            s = " ".join(s.split(" ")[1:])
-        i = ImageGen(bing_cookie)
-        limit = i.get_limit_left()
-        if limit < 2:
-            bot.reply_to(message, "We can not use it, cause we have no limit here")
+        # Find an available cookie
+        global bing_cookie
+        for cnt in range(len(bing_cookie)):
+            i = ImageGen(bing_cookie[cnt])
+            limit = i.get_limit_left()
+            if limit > 1:
+                break
+            i = None
+            bot.reply_to(message, "We can not use it, cause we have no limit here. Tring next cookie...")
+
+        if not i:
+            bot.reply_to(message, "No cookie is with limit left.")
+            return
         else:
+            # Rotate the cookie list so that the next time we use the available cookie
+            bing_cookie = bing_cookie[cnt:] + bing_cookie[:cnt]
             bot.reply_to(
                 message,
                 f"Using bing DALL-E 3 generating images please wait, left times we can use: {limit-1}",
             )
+        # Generate the images
         try:
             images = i.get_images(s)
         except Exception as e:
             print(str(e))
             bot.reply_to(message, "Ban from Bing DALL-E 3")
             return
-        photos_list = [InputMediaPhoto(i) for i in images]
+        # Save the images locally
         Thread(target=save_images, args=(i, images, path)).start()
+        # Send the images
+        photos_list = [InputMediaPhoto(i) for i in images]
         if photos_list:
             bot.send_media_group(
                 message.chat.id,
